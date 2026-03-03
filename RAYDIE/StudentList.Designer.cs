e50ddee5c339b47bdfbe182b0e614978483d6f3d
kiddo-wkwkwk <iamxnatoy@gmail.com>
2026-03-03 14:20:15 +0800

    Add files via upload

---
+++ RAYDIE/StudentList.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace RAYDIE
+{
+    partial class StudentList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox6 = new TextBox();
+            button3 = new Button();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label7 = new Label();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(109, 135);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(310, 288);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(109, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 15);
+            label1.TabIndex = 1;
+            label1.Text = "STUDENT MASTER LIST";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(270, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "SEARCH";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(344, 54);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "REFRESH";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(606, 244);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 23;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(606, 176);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(606, 213);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(606, 142);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(606, 103);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 19;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(523, 287);
+            button3.Name = "button3";
+            button3.Size = new Size(183, 23);
+            button3.TabIndex = 18;
+            button3.Text = "SAVE RECORD";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(558, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 15);
+            label6.TabIndex = 17;
+            label6.Text = "Age:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(540, 216);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 15);
+            label5.TabIndex = 16;
+            label5.Text = "Section:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(542, 179);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 15);
+            label4.TabIndex = 15;
+            label4.Text = "Course:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(523, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 15);
+            label3.TabIndex = 14;
+            label3.Text = "Last Name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(523, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 13;
+            label2.Text = "First Name:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(523, 62);
+            label7.Name = "label7";
+            label7.Size = new Size(194, 15);
+            label7.TabIndex = 12;
+            label7.Text = "REGISTER STUDENT INFORMATION";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(523, 316);
+            button4.Name = "button4";
+            button4.Size = new Size(183, 23);
+            button4.TabIndex = 24;
+            button4.Text = "DELETE RECORD";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // StudentList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox6);
+            Controls.Add(button3);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label7);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "StudentList";
+            Text = "StudentList";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox6;
+        private Button button3;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label7;
+        private Button button4;
+    }
+}
